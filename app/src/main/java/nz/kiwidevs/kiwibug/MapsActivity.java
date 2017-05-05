b1c648740280a886c9e04ae954335eecc5e4d34f
CCninja86 <james.nz@live.com>
2017-05-05 16:21:32 +1200

    - Stuff

--- app/src/main/java/nz/kiwidevs/kiwibug/MapsActivity.java
+++ app/src/main/java/nz/kiwidevs/kiwibug/MapsActivity.java
@@ -1,23 +1,39 @@
 package nz.kiwidevs.kiwibug;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, NewUserFragment.OnFragmentInteractionListener {
 
-    private GoogleMap mMap;
+    //private GoogleMap mMap;
+    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("nz.kiwidevs.kiwibug.FIRST_TIME", Context.MODE_PRIVATE);
+        boolean firstTime = sharedPreferences.getBoolean("first_time", true);
+
+        if(firstTime){
+            NewUserFragment newUserFragment = new NewUserFragment();
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.container, newUserFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("first_time", false);
+            editor.commit();
+        }
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -36,11 +52,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+//        mMap = googleMap;
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    @Override
+    public void onFragmentInteraction(String action) {
+        if(action.equals("continue")){
+            getFragmentManager().popBackStackImmediate();
+        }
     }
 }
