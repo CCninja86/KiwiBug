e27d7b26316248f3abfa0d2c29e754011292a346
michael_d <michael.daschner@gmail.com>
2017-05-18 10:43:27 +1200

    Removed LocationHistory,  LocationHistoryCallback and TagLocationHelper

--- app/src/main/java/nz/kiwidevs/kiwibug/LocationHistoryCallback.java
+++
@@ -1,9 +0,0 @@
-package nz.kiwidevs.kiwibug;
-
-/**
- * Created by James on 13/05/2017.
- */
-
-public interface LocationHistoryCallback {
-    public void locationHistoryDownloadComplete(LocationHistory locationHistory);
-}
